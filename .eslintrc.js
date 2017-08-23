5dcd84c85022dbd0e0defc76e4eb1e2753dc0de1
hzzhangjinxin <hzzhangjinxin@corp.netease.com>
2017-08-23 21:17:13 +0800

    misc: eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    "semi": [2, "always"], // 要求在行末加上分号
+    "indent": [2, 4],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
